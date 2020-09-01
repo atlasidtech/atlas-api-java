4a81141c6be037820c8f2aa9f274f20c6ce7ccd5
Hendri Antomy <hendri.antomy@mokapos.com>
2020-09-01 18:38:20 +0700

    add sample for intermediate class for CRUD customer

--- src/test/java/ApiJavaAutomationTest.java
+++ src/test/java/ApiJavaAutomationTest.java
@@ -1,70 +1,123 @@
+import static io.restassured.RestAssured.given;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.*;
-
 public class ApiJavaAutomationTest {
-    final static String firstUrl = "http://dummy.restapiexample.com";
-    final static String secondUrl = "https://reqres.in";
 
-    /*
-        Hallo ini adalah initial state terkait java rest assured kita.
-        Coba run class ini, dan pastikan program berjalan dan menghasilkan
-        output "The response status is 200".
-        Segera kontak tim Atlas jika menemui kendala!
-
-        Nantikan berbagai latihan dan problem set di kelas nanti!
-     */
+    public final static String URL = "https://reqres.in/";
 
     @Test
-    public void firstTrial(){
-        Response response = given().baseUri(firstUrl).basePath("/api/v1").contentType(ContentType.JSON)
-                .get("/employees");
+    public void getListUsers() {
+        Response response = given().log().all().baseUri(URL).basePath("/api")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .queryParam("page", 2)
+            .get("/users");
+
+        response.getBody().prettyPrint();
 
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
-        System.out.println("The response status is " +statusCode);
+        Assert.assertEquals(response.path("page"), new Integer(2));
+        System.out.println("The response status is " + statusCode);
     }
+
 
     @Test
-    public void getResponseBody(){
+    public void getSingleUser() {
+        Response response = given().log().all().baseUri(URL).basePath("/api")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .pathParam("userId", 2)
+            .get("/users/{userId}");
 
+        response.getBody().prettyPrint();
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("The response status is " + statusCode);
     }
+
 
     @Test
-    public void getFirstEmployeeName(){
+    public void createUser() {
 
+        String requestBody = "{\n"
+            + "    \"name\": \"Tri Abror\",\n"
+            + "    \"job\": \"SEIT\"\n"
+            + "}";
+
+        Response response = given().log().all().baseUri(URL).basePath("/api")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .body(requestBody)
+            .post("/users");
+
+        response.getBody().prettyPrint();
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 201);
+        System.out.println("The response status is " + statusCode);
     }
+
 
     @Test
-    public void tryQueryParameters(){
+    public void updateUser() {
 
+        String requestBodyAdd = "{\n"
+            + "    \"name\": \"Tri Abror\",\n"
+            + "    \"job\": \"SEIT\"\n"
+            + "}";
+
+        String requestBodyUpdate = "{\n"
+            + "    \"name\": \"Hendri\",\n"
+            + "    \"job\": \"SEIT\"\n"
+            + "}";
+
+        Response responseAdd = given().log().all().baseUri(URL).basePath("/api")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .body(requestBodyAdd)
+            .post("/users");
+
+        responseAdd.getBody().prettyPrint();
+
+        Response responseUpdate = given().log().all().baseUri(URL).basePath("/api")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .body(requestBodyUpdate)
+            .pathParam("userId", responseAdd.path("id"))
+            .put("/users/{userId}");
+
+        responseUpdate.getBody().prettyPrint();
+
+        int statusCode = responseUpdate.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("The responseAdd status is " + statusCode);
     }
+
 
     @Test
-    public void tryPathParameters(){
+    public void deleteeUser() {
 
+        String requestBody = "{\n"
+            + "    \"name\": \"Tri Abror\",\n"
+            + "    \"job\": \"SEIT\"\n"
+            + "}";
+
+        Response responseAdd = given().log().all().baseUri(URL).basePath("/api")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .body(requestBody)
+            .post("/users");
+
+        responseAdd.getBody().prettyPrint();
+
+        Response responseDelete = given().log().all().baseUri(URL).basePath("/api")
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .pathParam("userId", responseAdd.path("id"))
+            .delete("/users/{userId}");
+
+        int statusCode = responseDelete.getStatusCode();
+        Assert.assertEquals(statusCode, 204);
+        System.out.println("The response status is " + statusCode);
     }
 
-    @Test
-    public void tryVerifyEmployee(){
-
-    }
-
-    @Test
-    public void postCreateUser(){
-
-    }
-
-    @Test
-    public void updateUser(){
-
-    }
-
-    @Test
-    public void deleteEmployee(){
-
-    }
 }
